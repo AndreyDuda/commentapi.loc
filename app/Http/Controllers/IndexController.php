4f7eb1ed048ff2a7f7fbc05f0e6b6825c4d08e0d
AndreyDuda <AndreyDuda88@gmail.com>
2018-08-21 23:16:46 +0300

    refactoring PSR

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Repositories\CommentsRepository;
 
-
 class IndexController extends SiteController
 {
     public function __construct(CommentsRepository $commentsRep)
